d3ec6d09cc63cce7e234befcd407d899eadbaca4
BarnaliBhowmik <143362520+BarnaliBhowmik@users.noreply.github.com>
2025-09-07 12:18:47 +0530

    added 2 files

--- BMI.py
+++ BMI.py
@@ -6,3 +6,17 @@ bmi = weight/(height**2)
 
 print(round(bmi))
 # print(round(bmi, 2)) - It round's BMI to 2 decimal places
+
+
+# BMI CALCULATOR WITH INTERPRETATIONS 👇🏻
+
+weight1 = 85
+height2 = 1.85
+
+BMI = weight1 / (height2 ** 2)
+if BMI < 18.85:
+    print("underweight")
+elif 18.5 <= BMI < 25:
+    print("normal weight")
+else:
+    print("overweight")
